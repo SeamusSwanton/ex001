40cfd60cc948a2136624818386bbdac865bf9c77
1005307004 <1005307004@L829WD210013.edu.cbe.ab.ca>
2022-09-12 13:28:16 -0600

    utensil is done

--- Exercises OO2/src/ex005/UtensilCheck.java
+++ Exercises OO2/src/ex005/UtensilCheck.java
@@ -1,23 +1,47 @@
 package ex005;
 
 import ex003.MenuItem;
+import ex003.Salad;
+import ex003.Sandwich;
+import ex004.Cappuccino;
+import ex003.Drink;
 
 public class UtensilCheck {
 	
 	public static boolean requireFork(MenuItem item) {
-		return false;
+		if(item instanceof Salad || item instanceof Sandwich) {
+			return true;
+		}
+		else {
+			return false;
+		}		
 	}
 	
 	public static boolean requireKnife(MenuItem item) {
-		return false;
+		if(item instanceof Sandwich) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public static boolean requireSpoon(MenuItem item) {
-		return false;
+		if(item instanceof Cappuccino) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public static boolean requireStraw(MenuItem item) {
-		return false;
+		if(item.getClass() == Drink.class) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
